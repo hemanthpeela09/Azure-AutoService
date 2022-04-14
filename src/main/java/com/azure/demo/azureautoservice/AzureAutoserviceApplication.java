041d1abfab1123d3d7b2e74891d25af298518d2e
Hemanth Peela <hemanthpeela09@gmail.com>
2022-04-14 18:11:48 +0530

    Azure changes update

---
+++ src/main/java/com/azure/demo/azureautoservice/AzureAutoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.azure.demo.azureautoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAutoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureAutoserviceApplication.class, args);
+	}
+
+}
